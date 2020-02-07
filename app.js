9bb53d0c31e1dcae99281ff5465285bb0d806694
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-07 09:20:07 -0800

    update

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 'use strict';
 
-const Manager = require ('./lib/Manager');
-const Engineer = require ('./lib/Engineer');
-const Intern = require ('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 //const html = require('./template/generateHtml');
-const fs = require ('fs');
-const prompt = require ('./lib/prompt');
+const fs = require('fs');
+const prompt = require('./lib/prompt');
 
-async function init(){
-    try{
-        const ManagerInput = await prompt.addManager();
+async function init() {
+    try {
+        await prompt.enterPrompt();
     }
-    catch(error){
+    catch (error) {
         console.error(error);
     }
-    await prompt.chooseMember();
+
 }
- 
 
 
 
-init()
+
+init();
