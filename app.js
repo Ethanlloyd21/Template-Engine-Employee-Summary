12835adcb42a8b15c9c12171ab349a4153751e0e
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-08 13:43:35 -0800

    update code

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const fs = require('fs');
 const prompt = require('./lib/prompt');
 
 async function init() {
