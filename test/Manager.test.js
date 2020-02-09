6327f70b5ca880db15927f555154cc6f6ef0eeee
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-09 15:33:08 -0800

    test completed

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,12 +4,12 @@ const Manager = require('../lib/Manager');
 
 test('Checking for office number via constructor argument', () => {
     const value = 111;
-    const manager = new Manager('Lloyd', 789, 'test@only.com', testValue);
+    const manager = new Manager('Lloyd', 789, 'test@only.com', value);
     expect(manager.officeNumber).toBe(value);
 });
 
-test('Checking for office number via getOffice()', () => {
+test('Checking for office number via getofficeNumber()', () => {
     const value = 111;
     const manager = new Manager('Foo', 789, 'test@test.com', value);
-    expect(manager.getOfficeNumber()).toBe(value);
+    expect(manager.getofficeNumber()).toBe(value);
 });
